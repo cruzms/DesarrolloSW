2fa451191776334333d62740ed2190477242050c
Santiago Cruz <santiago.1701412936@ucaldas.edu.co>
2018-04-28 19:41:39 -0500

    Commit Inicial

---
+++ README.md
@@ -0,0 +1,24 @@
+# Proyecto Desarrollo Software
+
+### Deben tener instalado
+* [Node JS](https://nodejs.org/dist/v8.11.1/node-v8.11.1-x64.msi)
+* [Git](https://git-scm.com/downloads)
+* [MongoDB](https://www.mongodb.com/download-center#community) -Opcional para hacer pruebas locales
+
+### Para clonar el repositorio
+
+En la consola de git
+
+```
+git clone https://github.com/cruzms/DesarrolloSW.git
+```
+
+Después de haberlo clonado, en la carpeta del proyecto usando el cmd ejecutar
+
+```
+npm install
+```
+
+```
+npm install -g nodemon
+```
---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://sqladmin:cisco123@ds257589.mlab.com:57589/testswdb');
+// mongoose.connect('mongodb://localhost:27017/testDB');
+const db = mongoose.connection;
+// si se conecto
+db.once('open', () => {
+    console.log('DB conectada');
+});
+
+// si hay errores
+db.on('error', (err) => {
+    console.log('Error DB');
+});
+
+const app = express();
+const indexRouter = require('./routes/index');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter); 
+
+app.listen(app.get('port'), () => {
+    console.log('Server port 3000')
+});
